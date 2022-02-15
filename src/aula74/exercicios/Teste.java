de5e524aea0b3dafc83a2d107978c3b5dcbd71ca
colinbruna <brunacbaldin@gmail.com>
2022-02-15 20:43:16 -0300

    exercicio aula 74 simulador semaforo

--- src/aula74/exercicios/Teste.java
+++ src/aula74/exercicios/Teste.java
@@ -2,9 +2,33 @@ package aula74.exercicios;
 
 public class Teste {
 
+    public enum CorSemafaro {
 
+        VERDE(3000), AMARELO(500), VERMELHO(1500);
 
+        private int tempoEspera;
 
+        CorSemafaro(int tempoEspera){
+            this.tempoEspera = tempoEspera;
+        }
 
+        public int getTempoEspera() {
+            return tempoEspera;
+        }
 
+        public void setTempoEspera(int tempoEspera) {
+            this.tempoEspera = tempoEspera;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        ThreadSemaforo semaforo = new ThreadSemaforo();
+
+        for (int i=0; i<10; i++) {
+            System.out.println(semaforo.getCorSemafaro());
+            semaforo.esperaCorMudar();
+        }
+        semaforo.desligarSemafaro();
+    }
 }
--- src/aula74/exercicios/ThreadSemaforo.java
+++ src/aula74/exercicios/ThreadSemaforo.java
@@ -1,6 +1,66 @@
 package aula74.exercicios;
 
-public class ThreadSemaforo {
+public class ThreadSemaforo implements Runnable {
 
+    private Teste.CorSemafaro corSemafaro;
+    private boolean continuar;
+    private boolean mudancaCor;
 
+    public ThreadSemaforo() {
+        this.corSemafaro = Teste.CorSemafaro.VERDE;
+
+        this.continuar = false;
+        this.mudancaCor = false;
+
+        new Thread(this).start();
+    }
+
+    @Override
+    public void run() {
+        while (!continuar) {
+            try {
+                Thread.sleep(this.corSemafaro.getTempoEspera());
+                this.mudancaCor();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private synchronized void mudancaCor() {
+        switch (this.corSemafaro) {
+            case VERDE:
+                this.corSemafaro = Teste.CorSemafaro.AMARELO;
+                break;
+            case AMARELO:
+                this.corSemafaro = Teste.CorSemafaro.VERMELHO;
+                break;
+            case VERMELHO:
+                this.corSemafaro = Teste.CorSemafaro.VERDE;
+                break;
+            default:
+                break;
+        }
+        this.mudancaCor = true;
+        notify();
+    }
+
+    public synchronized void esperaCorMudar(){
+        while(!this.mudancaCor){
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        this.mudancaCor = false;
+    }
+
+    public synchronized void desligarSemafaro(){
+        this.continuar = true;
+    }
+
+    public Teste.CorSemafaro getCorSemafaro() {
+        return corSemafaro;
+    }
 }
